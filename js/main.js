6fee2ea7f6752a7272ac378e92127b9e76fd0ba9
Marco 9726 <marcosarra9@gmail.com>
2022-12-14 18:33:44 +0100

    nel for definisco i tag html da inserire nel DOM

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//1 creo array di oggetti letterali che contengono : url dell'immagine, titolo e descrizione
+//creo array di oggetti letterali che contengono : url dell'immagine, titolo e descrizione
 const mainArray = [
     {
         img: '01.webp',
@@ -26,3 +26,25 @@ const mainArray = [
         descrition: 'Marvels Avengers è un videogioco action-adventure sviluppato dalla Crystal Dynamics e Eidos Montréal, e pubblicato dalla Square Enix.'
     }
 ]
+
+//creo un for per ciclare gli oggetti dell'array
+for(i=0;i<mainArray.length;i++){
+    //creo i div che conterranno le immagini grandi
+    const bigImgDiv = document.createElement('div');
+    //assegno la classe item(display none) al div appena creato
+    bigImgDiv.classList.add('item');
+    //creo l'elemento HTML img
+    const tagImg = document.createElement('img');
+    //assegno ad una costante il valore degli oggetti
+    const game = mainArray[i];
+    //assegno in una costante il valore della proprietà 'img' degli oggetti
+    const img = game.img;
+    //rintraccio il percorso dell'immagine grazie alla costante appena creata
+    tagImg.src =`./img/${img}`;
+    //inserisco le img nei div
+    bigImgDiv.appendChild(tagImg);
+    //inserisco i div nell'HTML
+    document.getElementById('big-img-container').appendChild(bigImgDiv);
+
+    
+}
