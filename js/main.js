16576e3732d95b8f9c73f09ea72d5fc4bc8e6e7d
Marco 9726 <marcosarra9@gmail.com>
2022-12-14 17:57:07 +0100

    creazione array di oggetti

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//1 creo array di oggetti letterali che contengono : url dell'immagine, titolo e descrizione
+const mainArray = [
+    {
+        img: '01.webp',
+        title: 'Marvel Spiderman Miles Morales',
+        descrition: 'Il giovane Miles Morales sta cercando di adattarsi alla nuova realtà mentre, nel ruolo di nuovo Spider-Man, segue i passi del suo mentore.'
+    },
+    {
+        img: '02.webp',
+        title: 'Ratchet & Clanl: Rift Apart',
+        descrition: 'Ratchet & Clank: Rift Apart è un videogioco a piattaforme e sparatutto in terza persona sviluppato da Insomniac Games e pubblicato da Sony Interactive Entertainment per PlayStation 5.'
+    },
+    {
+        img: '03.webp',
+        title: 'Fortnite',
+        descrition: 'Fortnite è un videogioco sparatutto in terza persona del 2017, sviluppato da People Can Fly e pubblicato da Epic Games per console e PC.'
+    },
+    {
+        img: '04.webp',
+        title: ' Stray',
+        descrition: 'Stray è un videogioco di tipo avventura dinamica del 2022 sviluppato da BlueTwelve Studio e pubblicato dall Annapurna Interactive per PlayStation 4, PlayStation 5 e Microsoft Windows.'
+    },
+    {
+        img: '05.webp',
+        title: 'Marvel Avengers',
+        descrition: 'Marvels Avengers è un videogioco action-adventure sviluppato dalla Crystal Dynamics e Eidos Montréal, e pubblicato dalla Square Enix.'
+    }
+]
